98a1a3cff09992fdfb2e6ce04dd392deb9fb85b7
e <abdulmonemalaaaldeen@gmail.com>
2024-10-28 11:20:37 +0300

    Include Bootstrap Files In Project

--- resources/views/blog/layouts/master.blade.php
+++ resources/views/blog/layouts/master.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <title>@yield('title')</title>
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <link href="{{asset('css/bootstrap.min.css')}}" rel="stylesheet">
 </head>
 
 <body>
@@ -65,9 +65,7 @@
 </footer>
 
 
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.0.6/dist/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<script src="{{asset('js/bootstrap.min.js')}}"></script>
 </body>
 
 </html>
